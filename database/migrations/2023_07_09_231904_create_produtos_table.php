866705d8770d131f4ebfc219f2b8d38043543dd3
Carlos <carlos.santos@ifsuldeminas.edu.br>
2023-07-23 17:30:45 -0300

    Projeto Final

--- database/migrations/2023_07_09_231904_create_produtos_table.php
+++ database/migrations/2023_07_09_231904_create_produtos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer('qtd');
             $table->float('precoUnitario');
             $table->float('precoVenda');
+            $table->timestamps();
         });
     }
 
